bdfc498385c12431f23217ccb81998ddeba4bbd8
marcus <marcus-pf@hotmail.com>
2016-12-18 15:58:19 -0200

    Testing connection on socket

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-    });
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
     });
