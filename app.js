91a5a1287a98750450b40224f79062eea0812845
marcus <marcus-pf@hotmail.com>
2016-12-18 20:25:16 -0200

    Manage users online

--- app.js
+++ app.js
@@ -1,20 +1,54 @@
 /**
  * Created by marcus on 18/12/16.
  */
+var count = 0;
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+var username = [];
 
-app.get('/', function(req, res){
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.post('/login', function(req, res){
+    username.push(req.body.username);
+    res.redirect("/chat");
+});
+
+app.get('/chat', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-    });
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/login.html');
 });
+
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+    var user = username[count];
+    var id = count;
+    count++;
+    io.emit('conect', username);    //envia a lista de usuário onlines para o cliente
+    io.emit('chat message', "usuário " + user + " entrou na conversa");
+
+
+    socket.on('disconnect', function(){
+        io.emit('chat message', "Usuário " + user + " saiu da conversa");
+        count --;
+        username.splice(id,1);
+        io.emit("update", username);
+
+    });
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
 });
