f3990f78bb1d9f8347be12629e780a39f45521b0
marcus <marcus-pf@hotmail.com>
2016-12-20 08:45:37 -0200

    Document code and update socket array in disconnect event

--- app.js
+++ app.js
@@ -37,27 +37,36 @@ http.listen(3000, function(){
 });
 
 io.sockets.on('connection', function(socket){
-    var user = username[count];
-    var id = count;
-    sockets.push(socket.id); // guarda o id do soket
-    count++;                 //atualiza o número de usuário conectados
+    var user = username[count]; // guarda o username do usuario
+    var id = count;             // guarda o id do usuário
+    sockets.push(socket.id);    // guarda o id do soket
+    count++;                    // atualiza o número de usuário conectados
 
     io.emit('conect', username, user);    //envia a lista de usuário onlines para o cliente
 
     socket.on('disconnect', function(){
-        io.emit('leave chat', user + " saiu da conversa");
-        count --;
-        username.splice(id,1);
-        io.emit("update", username);
+        io.emit('leave chat', user + " saiu da conversa"); // emite uma mensagem para os demais clientes avisando a saida do chat
+        count --;                                          // atualiza o número de usuário conectados
+        username.splice(id,1);                             // remove o username da lista de usuários conectados
+        sockets.splice(id, 1);                             // remove o socket da lista de sockets conectados
+        io.emit("update", username);                       // atualiza os clientes
 
     });
 
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg, user);
+    socket.on('chat message', function(msg){    // emite uma mensagem para todos os clientes conectados
+        io.emit('chat message', msg, user);     // informando a mensagem e o usuário que a enviou
     });
 
     socket.on('private', function (msg,to){
         var position = username.indexOf(to);
+        /*
+        *** Evento de mensagem privada ***
+        * Recebemos como parâmetro a mensagem e o username do usuário de destino
+        * buscamos o username no vetor de usernames e pegamos a posição em que ele se encontra
+        * pegamos o id do socket que aquele usuário está conectado
+        * enviamos uma mensagem apenas para o usuário destino
+        * enviamos uma mensagem para o próprio emissor também
+        */
         console.log(username);
         console.log(position);
         console.log(sockets);
