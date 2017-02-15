0003e9045f86c75ea4eb06f8c7b607d431f9f481
elisamarcatto <elisamarcatto@gmail.com>
2017-02-15 18:12:30 -0200

    Added schema and model

--- app.js
+++ app.js
@@ -6,15 +6,38 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/messages');
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function() {
+});
+
+var messageSchema = mongoose.Schema({
+    timestamp: Date,            
+    text: String,
+    user: String          
+});
+
+var Message = mongoose.model('Message', messageSchema);
+
+var historySchema = mongoose.Schema({
+    socket_id: String,
+    c_id: String,
+    p_id: String,
+    messages: String
+});
+
+var History = mongoose.model('History', historySchema);
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-
 app.post('/login', function(req, res){
-    username.push(req.body.username);
     var chatName = req.body.chatName;
     createSocket(chatName);
     res.redirect("/chat/" + chatName);
@@ -33,37 +56,33 @@ http.listen(3000, function(){
     console.log('listening on *:3000');
 });
 
-
 function createSocket(name) {
-    var count = 0;
-    var username = [];
-    var sockets = [];
-    var io2 =io.of('/' + name);
-    io2.on('connection', function(socket){
-        var user = username[count]; // guarda o username do usuario
-        var id = count;             // guarda o id do usuário
-        sockets.push(socket.id);    // guarda o id do soket
-        count++;                    // atualiza o número de usuário conectados
+  
+    var namespace = io.of('/' + name);
+    namespace.on('connection', function(socket){
 
+        var c_id = namespace.split('p')[0];
+        var p_id = namespace.split('p')[1];
 
-        console.log(user + " . Socket id: " + socket.id);
-
-        io2.emit('conect', username, user);    //envia a lista de usuário onlines para o cliente
-
-        socket.on('disconnect', function(){
-            io2.emit('leave chat', user + " saiu da conversa"); // emite uma mensagem para os demais clientes avisando a saida do chat
-            count --;                                          // atualiza o número de usuário conectados
-            username.splice(id,1);                             // remove o username da lista de usuário2s conectados
-            sockets.splice(id, 1);                             // remove o socket da lista de sockets conectados
-            io2.emit("update", username);                       // atualiza os clientes
-
+        var historyDocument = new History({
+            socket_id: 'name',
+            c_id: 'c_id',
+            p_id: 'p_id',
+            messages: 'null'
         });
+
+        historyDocument.save();
 
         socket.on('chat message', function(msg){    // emite uma mensagem para todos os clientes conectados
-            io2.emit('chat message', msg, user);     // informando a mensagem e o usuário que a enviou
+            namespace.emit('chat message', msg);     // informando a mensagem e o usuário que a enviou
+            console.log(msg);
+
+            var message = new Message({
+                timestamp: new Date(),            
+                text: msg,
+                user: 'das' 
+            });           
         });
 
-
     });
-
 }
