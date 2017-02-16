12a99a7cd023e1a94ba209fdf1aad5548684bb47
marcus <marcus-pf@hotmail.com>
2017-02-16 16:53:31 -0200

    Request token by ajax

--- app.js
+++ app.js
@@ -42,6 +42,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();});
+
 app.post('/login', function(req, res){
     var chatName = req.body.chatName;
     var token = req.body.token;
