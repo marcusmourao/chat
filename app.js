d6038e7aafa88c38187c1e78df36dd31c160394a
marcus <marcus-pf@hotmail.com>
2017-02-22 14:02:42 -0300

    Testing namespace

--- app.js
+++ app.js
@@ -1,140 +1,80 @@
 /**
  * Created by marcus on 18/12/16.
  */
-var express = require('express');
-var app = express();
+var count = 0;
+var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
-var socketioJwt = require('socketio-jwt');
-var usernames = [];
-var tokens =[];  // variavel global para empilhar o token
-
-app.set('view engine', 'ejs');  //view engine para passar parâmetros para a view
-app.use(express.static(__dirname + '/public'));
-
-
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/messages');
-
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function() {
-});
-
-var historySchema = mongoose.Schema({
-    socket_id: String,
-    c_id: String,
-    p_id: String,
-    messages: [{
-        last_date: String,
-        text: String,
-        user: String
-    }]
-});
-
-var History = mongoose.model('History', historySchema);
+var username = [];
+var sockets = [];
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();});
 
 app.post('/login', function(req, res){
-    var chatName = req.body.chatName;
-    var token = req.body.token;
-    var username = req.body.username;
-    tokens.push(token);
-    console.log(username);
-    usernames.push(username);
-    saveSocket(chatName);
-    entrySocket(chatName);
-    res.redirect("/chat/" + chatName);
+    username.push(req.body.username);
+    res.redirect("/chat");
 });
 
-app.get('/chat/:chatName', function(req, res){
-    res.render(__dirname + '/index.ejs', {token: tokens.pop(), username: usernames.pop()});
+app.get('/chat', function(req, res){
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/login.html');
 });
 
-app.get('/message', function(req, res){
-    res.sendFile(__dirname + '/message.html');
+app.get('/private', function (req, res) {
+
 });
 
-
-http.listen(8080, function(){
-    console.log('listening on *:8080');
+http.listen(3000, function(){
+    console.log('listening on *:3000');
 });
 
-function saveSocket(name) {
-    var query = History.find({socket_id: name.toString()}, function (err, histories) {
-        if(histories.length == 0) {
-            var c_id = name.split('p')[0];
-            var p_id = 'p' + name.split('p')[1];
-            var historyDocument = new History({
-                socket_id: name.toString(),
-                c_id: c_id.toString(),
-                p_id: p_id.toString(),
-                messages: {
-                    last_date: new Date().toString(),
-                    text: 'first message',
-                    user: 'null'
-                }
-            });
-            historyDocument.save();
-        }
-    });
-}
+var nsp = io.of('/mynamespace');
+nsp.on('connection', function(socket){
+    var user = username[count]; // guarda o username do usuario
+    console.log(user + "conectou");
+    var id = count;             // guarda o id do usuário
+    sockets.push(socket.id);    // guarda o id do soket
+    count++;                    // atualiza o número de usuário conectados
 
-function entrySocket(name) {
-    var namespace = io.of('/' + name);
-     namespace.on('connection', socketioJwt.authorize({
-        secret: 'vaiternespressosimounaomefalaquerosaber',
-        timeout: 15000 // 15 seconds to send the authentication message
-    })).
+    nsp.emit('conect', username, user);    //envia a lista de usuário onlines para o cliente
 
-     on('authenticated', function(socket) {
-        //this socket is authenticated, we are good to handle more events from it.
-        // console.log('hello!' + socket.decoded_token.id);
-         socket.removeAllListeners();
-         socket.on('chat message', function (msg, username) {    // emite uma mensagem para todos os clientes conectados
-             namespace.emit('chat message', msg, username);     // informando a mensagem e o usuário que a enviou
-             var query = History.find({socket_id: name.toString()}, function (err, histories) {
-                 histories[0].messages.push({
-                     last_date: new Date().toString(),
-                     text: msg,
-                     user: username
-                 });
-                 histories[0].save();
-                 // console.log(histories[0].messages);
-             });
-         });
+    socket.on('disconnect', function(){
+        nsp.emit('leave chat', user + " saiu da conversa"); // emite uma mensagem para os demais clientes avisando a saida do chat
+        count --;                                          // atualiza o número de usuário conectados
+        username.splice(id,1);                             // remove o username da lista de usuários conectados
+        sockets.splice(id, 1);                             // remove o socket da lista de sockets conectados
+        nsp.emit("update", username);                       // atualiza os clientes
+
     });
 
+    socket.on('chat message', function(msg){    // emite uma mensagem para todos os clientes conectados
+        nsp.emit('chat message', msg, user);     // informando a mensagem e o usuário que a enviou
+    });
 
+    socket.on('private', function (msg,to){
+        var position = username.indexOf(to);
+        /*
+         *** Evento de mensagem privada ***
+         * Recebemos como parâmetro a mensagem e o username do usuário de destino
+         * buscamos o username no vetor de usernames e pegamos a posição em que ele se encontra
+         * pegamos o id do socket que aquele usuário está conectado
+         * enviamos uma mensagem apenas para o usuário destino
+         * enviamos uma mensagem para o próprio emissor também
+         */
+        console.log(username);
+        console.log(position);
+        console.log(sockets);
+        console.log(sockets[position]);
+        socket.broadcast.to(sockets[position]).emit('private message', msg, user);
+        socket.emit('personal', msg, user, to);
+    });
 
-
-}
+});
